0d2cf34b75e3751e27489d6689851556ea38cb5a
luangouveiax <luan.gouveiax@gmail.com>
2025-01-13 00:56:21 -0300

    Twig e Templates

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Loja estelar</strong>: sua opção de acabar com o monopólio de peças de naves especiais!');
+        $qtdNaves = 457;
+        $minhaNave = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'em construção',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'qtdNaves' => $qtdNaves,
+            'minhaNave' => $minhaNave
+        ]);
     }
 }
